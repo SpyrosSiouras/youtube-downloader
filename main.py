e6f1aae6a2e9b85fbae3de15926d8b603db5897d
spyros <siourasspyros@gmail.com>
2023-04-18 15:44:18 +0300

    upgraded pytube, because of bug

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from pytube import YouTube
 
 
 if __name__ == "__main__":
-    ...
     
+    link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
+
+    yt = YouTube(link)
+
+    print(f"Title: {yt.title}")
