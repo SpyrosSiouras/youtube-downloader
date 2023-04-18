a1d57da6c35225b3e875018b791f11b7eadebe4b
spyros <siourasspyros@gmail.com>
2023-04-18 16:07:06 +0300

    added try exception in main, modified cipher regex in pytube

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pytube import YouTube
-
+from pytube.exceptions import PytubeError
 
 if __name__ == "__main__":
     
     link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
 
-    yt = YouTube(link)
-
-    print(f"Title: {yt.title}")
+    try:
+        yt = YouTube(link)
+        print(f"Title: {yt.title}")
+    
+    except PytubeError: 
+    
+        print(f"Could not get youtube link! ")
