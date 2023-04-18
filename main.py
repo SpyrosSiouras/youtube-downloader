f5a3378e4d2e51407606ad641e9176162b768d00
spyros <siourasspyros@gmail.com>
2023-04-18 18:07:08 +0300

    fixed requirements packages

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from pytube.exceptions import PytubeError
 if __name__ == "__main__":
     
     link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
+    yt = None
 
-    try:
-        yt = YouTube(link)
-        print(f"Title: {yt.title}")
+    while yt is None: 
+        try:
+            yt = YouTube(link)
+            print(yt.title)
+
+        except PytubeError: 
+            yt = None
+            print(f"Could not get youtube link! ")
     
-    except PytubeError: 
     
-        print(f"Could not get youtube link! ")
+    print(f"Title: {yt.title}")
