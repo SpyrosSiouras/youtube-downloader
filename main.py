78e0d2aabca9c7c201fe8f0b303aca60ee3da68e
spyros <siourasspyros@gmail.com>
2023-04-19 16:54:30 +0300

    new module for results set, test example of flet

--- main.py
+++ main.py
@@ -1,23 +1,87 @@
-from pytube import YouTube
-from pytube.exceptions import PytubeError
+# -*- coding: utf-8 -*-
+"""
 
-if __name__ == "__main__":
-    
-    # link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
-    link: str = "https://www.youtube.com/watch?v=UWeAyFrfWz0"
-    yt = None
+@author: PythonDude
+"""
 
-    while yt is None: 
-        try:
-            yt = YouTube(link)
-            print(yt.title)
+import flet as ft
 
-        except PytubeError: 
-            yt = None
-            print(f"Could not get youtube link! ")
+class TasksApp(ft.UserControl):
     
+    def build(self):
+        self.textField=ft.TextField(width=350, on_submit=self.addClick)
+        self.addBtn   = ft.FloatingActionButton(icon= ft.icons.ADD,
+                                                on_click= self.addClick)
+        self.tasks = ft.Column()
+        taskRow = ft.Column(controls=[
+            ft.Row(alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                   controls=[self.textField, self.addBtn]),
+            self.tasks])
+        return taskRow
     
-    print(f"Title: {yt.title}")
-    # print(f"streams : {yt.streams.filter(only_audio = True)}")
-    stream = yt.streams.get_by_itag(251)
-    stream.download()
+    def addClick(self, e):
+        task=Task(self.textField.value, self.taskDelete)
+        self.tasks.controls.append(task)
+        self.textField.value=""
+        self.update()
+    
+    def taskDelete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+        
+class Task(ft.UserControl):
+    def __init__(self, taskName, taskDelete):
+        super().__init__()
+        self.taskName= taskName
+        self.taskDelete=taskDelete
+        
+    def build(self):
+        self.displayTask=ft.Checkbox(label=self.taskName,value=False)
+        self.editName = ft.TextField()
+        
+        self.displayView = ft.Row(alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                                  controls=[self.displayTask,
+                                            ft.Row(controls=[ft.IconButton(ft.icons.CREATE_OUTLINED,
+                                                                           on_click=self.editClick),
+                                                             ft.IconButton(ft.icons.DELETE_OUTLINED,
+                                                                           on_click=self.deleteClick)])])
+        self.editView = ft.Row(alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                               visible=False,
+                               controls=[self.editName,
+                                         ft.IconButton(ft.icons.DONE_OUTLINED,
+                                                                           on_click=self.saveClick)])
+        return ft.Column(controls=[self.displayView, self.editView])
+    def editClick(self, e):
+        self.displayView.visible=False
+        self.editView.visible=True
+        self.editName.value= self.displayTask.label
+        self.update()
+    
+    def saveClick(self, e):
+        self.displayView.visible=True
+        self.editView.visible=False
+        self.displayTask.label=self.editName.value
+        self.update()
+        
+    
+    def deleteClick(self, e):
+        self.taskDelete(self)
+        
+        
+        
+        
+
+def main(page: ft.page):
+    page.title="Tasking App By PythonDude"
+    page.window_width=500
+    page.window_height=700
+    page.bgcolor ="BLUE"
+    
+    # taskingApp =TasksApp()
+    # page.add(taskingApp)
+ 
+    
+ 
+ 
+    
+ft.app(target=main)
---
+++ ytb.py
@@ -0,0 +1,44 @@
+from pytube import YouTube, Search
+from pytube.exceptions import PytubeError
+
+def get_query_results(query: str) -> list[YouTube]:
+
+    try:
+        s = Search(query)
+        return s.results
+    except PytubeError as e:
+        print(f"EXCEPTION: {e}")
+        return []
+
+
+if __name__ == "__main__":
+
+    query_results = get_query_results("")
+    print(len(query_results))
+    for result in query_results:
+        print(result.thumbnail_url)
+    # # link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
+    # link: str = "https://www.youtube.com/watch?v=UWeAyFrfWz0"
+    # yt = None
+
+    # while yt is None: 
+    #     try:
+    #         yt = YouTube(link)
+    #         print(yt.title)
+
+    #     except PytubeError: 
+    #         yt = None
+    #         print(f"Could not get youtube link! ")
+    
+    
+    # print(f"Title: {yt.title}")
+    # print(f"streams : {yt.streams.filter(only_audio = True)[-1]}")
+    # # stream = yt.streams.get_by_itag(251)
+    # # stream.download()
+    # x = 0
+    # while x < 30: 
+    #     s = Search('Sido Astronaut')
+    #     print(f"{x}-{s.results[0].title}")
+    #     x += 1
+    #     time.sleep(0.1)
+    # s.results[0].streams[-1].download()
