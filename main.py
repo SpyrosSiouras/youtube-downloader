c9c3dcb32b6977d8f9c88382949da3a61ca78c43
spyros <spyrossgiovanis@gmail.com>
2023-04-19 11:32:45 +0300

    tested stream download

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from pytube.exceptions import PytubeError
 
 if __name__ == "__main__":
     
-    link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
+    # link: str = "https://www.youtube.com/watch?v=iEOSJeA2_48"
+    link: str = "https://www.youtube.com/watch?v=UWeAyFrfWz0"
     yt = None
 
     while yt is None: 
@@ -17,3 +18,6 @@ if __name__ == "__main__":
     
     
     print(f"Title: {yt.title}")
+    # print(f"streams : {yt.streams.filter(only_audio = True)}")
+    stream = yt.streams.get_by_itag(251)
+    stream.download()
